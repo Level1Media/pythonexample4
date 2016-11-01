ebcce091e66902637557194e4f0aab985cd14362
elihood <eli.hood@aol.com>
2016-11-01 19:35:10 +0000

    update

--- app/views.py
+++ app/views.py
@@ -1,4 +1,4 @@
-from flask import render_template, flash, redirect, url_for, request, session, abort ,g
+from flask import render_template, flash, redirect, url_for, request, session,escape, abort ,g
 from flask_login import login_user, logout_user, current_user, login_required, LoginManager
 from app import app, db
 from .models import User, Post
@@ -11,8 +11,9 @@ login_manager.login_view = 'login'
 
 @app.route('/')
 def index():
-	return render_template('index.html', title="home")
-
+    return render_template('index.html', title="home")
+    
+    
 
 @app.route('/register', methods=['GET','POST'])
 def register():
@@ -29,7 +30,8 @@ def register():
 def login():
     if request.method == 'GET':
         return render_template('login.html')
-    username = request.form['username']
+
+    username = session['username'] = request.form['username']
     password = request.form['password']
     registered_user = User.query.filter_by(username=username,password=password).first()
     if registered_user is None:
@@ -57,9 +59,10 @@ def before_request():
 
 @app.route('/posts')
 def posts():
- 
-    return render_template('/posts.html')
+       
+        return render_template("posts.html", posts=Post.query.all())
     
+
     
 @app.route('/posts/create_post', methods=['GET', 'POST'])
 def create_post():
@@ -72,6 +75,29 @@ def create_post():
         db.session.add(post)
         db.session.commit()
         return redirect(url_for('posts'))
+        
 
+
+
+@app.route('/posts/<int:id>')
+def show(id):
+    post = Post.query.get(id)
+    return render_template("post.html", pid=id)
     
     
+@app.route('/posts/delete/<int:id>')
+@login_required
+def delete(id):
+    post = Post.query.get(id)
+    if post is None:
+        flash('Post not found.')
+        return redirect(url_for('index'))
+        
+    db.session.delete(post)
+    db.session.commit()
+    flash('Your post has been deleted.')
+    return redirect(url_for('index'))
+    
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
